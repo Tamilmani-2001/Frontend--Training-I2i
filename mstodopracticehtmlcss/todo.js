8ed7a5f34ff727ff4d3ae70581e1c77b7ea31158
Tamilmani@2001 <tamilmani.kumarasamy@ideas2it.com>
2023-01-02 21:42:52 +0530

    js file modified

--- mstodopracticehtmlcss/todo.js
+++ mstodopracticehtmlcss/todo.js
@@ -1,5 +1,16 @@
+// (function () {
+//     function init() {
+function search(textElement) {
+    if (event.key == 'Enter') {
+        console.log(textElement.value);
+    }
+}
 function createListOrGroup() {
     if (document.getElementById("group")) {
         
     }
 }
+
+    //     }
+//     init();
+// })();
